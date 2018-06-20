3429945026e224d155e1735104a2f8d8e7618476
utkuufuk <utkuufuk@gmail.com>
2018-06-20 12:27:08 +0300

    add function to get the next result page

---
+++ scraper.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import argparse
+import requests
+
+SITE_URL = "https://old.reddit.com/"
+DEFAULT_KEYWORD = "jpeg%202000"
+AGENT = "Mozilla/5.0 Chrome/47.0.2526.106 Safari/537.36"
+LANGUAGE = 'en-US,en;q=0.8'
+
+def createSoup(url):
+    return BeautifulSoup(requests.get(url, headers={'User-Agent':AGENT, 'Accept-Language':LANGUAGE}).text, 'lxml')
+
+def getNextResultPageUrl(resultPageUrl):
+    nextPage = createSoup(resultPageUrl)
+    return nextPage.find('span', {'class':'nextprev'}).findAll('a')[-1]['href']
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--keyword', type=str, default=DEFAULT_KEYWORD, help="search keyword")
+    args = parser.parse_args()
+    searchUrl = SITE_URL + 'search?q="' + args.keyword + '"'
+
+    nextPageUrl = getNextResultPageUrl(searchUrl)
+    print(nextPageUrl)
+    
+    nextPageUrl = getNextResultPageUrl(nextPageUrl)
+    print(nextPageUrl)
