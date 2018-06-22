8b97323db9945951e5fac470f29bfd72dd4c84b3
utkuufuk <utkuufuk@gmail.com>
2018-06-22 19:11:09 +0300

    get results in all pages in a loop

--- scraper.py
+++ scraper.py
@@ -4,32 +4,25 @@ import requests
 
 SITE_URL = "https://old.reddit.com/"
 DEFAULT_KEYWORD = "jpeg%202000"
-AGENT = "Mozilla/5.0 Chrome/47.0.2526.106 Safari/537.36"
-LANGUAGE = 'en-US,en;q=0.8'
+REQUEST_AGENT = "Mozilla/5.0 Chrome/47.0.2526.106 Safari/537.36"
 
 def createSoup(url):
-    return BeautifulSoup(requests.get(url, headers={'User-Agent':AGENT, 'Accept-Language':LANGUAGE}).text, 'lxml')
+    return BeautifulSoup(requests.get(url, headers={'User-Agent':REQUEST_AGENT}).text, 'lxml')
 
-def getNextResultPageUrl(resultPageUrl):
-    page = createSoup(resultPageUrl)
-    footer = page.findAll('span', {'class':'nextprev'})[-1] 
-    return footer.findAll('a')[-1]['href']
+def getNextPageUrl(resultPageUrl):
+    resultPage = createSoup(resultPageUrl)
+    footers = resultPage.findAll('a', {'rel':'nofollow next'})
+    return footers[-1]['href']
 
 if __name__ == '__main__':
+    # build the search URL using the input keyword
     parser = argparse.ArgumentParser()
     parser.add_argument('--keyword', type=str, default=DEFAULT_KEYWORD, help="search keyword")
     args = parser.parse_args()
     searchUrl = SITE_URL + 'search?q="' + args.keyword + '"'
 
-    page = createSoup(searchUrl)
-    contents = page.findAll('div', {'class':'search-result-link'})
-
-    for content in contents:
-        print(content.text)
-        input() 
-
-    nextPageUrl = getNextResultPageUrl(searchUrl)
-    print(nextPageUrl)
-
-    nextPageUrl = getNextResultPageUrl(nextPageUrl)
-    print(nextPageUrl)
+    while True:
+        print("searching", searchUrl)
+        resultPage = createSoup(searchUrl)
+        results = resultPage.findAll('div', {'class':'search-result-link'})
+        searchUrl = getNextPageUrl(searchUrl)
