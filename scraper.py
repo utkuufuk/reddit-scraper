bb2698f7bab0870e6599e1009c6f6797af145b42
utkuufuk <utkuufuk@gmail.com>
2018-06-24 00:46:02 +0300

    compare posts with a date treshold

--- scraper.py
+++ scraper.py
@@ -4,33 +4,39 @@ import argparse
 import requests
 
 SITE_URL = "https://old.reddit.com/"
-DEFAULT_KEYWORD = "jpeg%202000"
+DEFAULT_KEYWORD = "tübitak"
 REQUEST_AGENT = "Mozilla/5.0 Chrome/47.0.2526.106 Safari/537.36"
+TRESHOLD_DATE = datetime(year=2017, month=1, day=1)
 
 def createSoup(url):
     return BeautifulSoup(requests.get(url, headers={'User-Agent':REQUEST_AGENT}).text, 'lxml')
 
-def getNextPageUrl(resultPageUrl):
-    resultPage = createSoup(resultPageUrl)
-    footers = resultPage.findAll('a', {'rel':'nofollow next'})
-    return footers[-1]['href']
-
-if __name__ == '__main__':
-    # build the search URL using the input keyword
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--keyword', type=str, default=DEFAULT_KEYWORD, help="search keyword")
-    args = parser.parse_args()
-    searchUrl = SITE_URL + 'search?q="' + args.keyword + '"'
-
+def getSearchResults(searchUrl):
+    searchResults = []
     while True:
-        print("searching", searchUrl)
         resultPage = createSoup(searchUrl)
         results = resultPage.findAll('div', {'class':'search-result-link'})
-        for result in results:
-            print(result.text)
-            time = result.find('time')
-            print(time['datetime'])
-            date = datetime.strptime(time['datetime'][:10], '%Y-%m-%d')
-            print(date)
-            input()
-        searchUrl = getNextPageUrl(searchUrl)
+        searchResults += results
+        footers = resultPage.findAll('a', {'rel':'nofollow next'})
+        if footers:
+            searchUrl = footers[-1]['href']
+        else:
+            return searchResults
+
+def processResults(results):
+    for result in results:
+        time = result.find('time')['datetime']
+        date = datetime.strptime(time[:10], '%Y-%m-%d')
+        if date < TRESHOLD_DATE:
+            return
+        title = result.find('a', {'class':'search-title'})
+        print(str(date)[:10] + ":", title.text)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--keyword', type=str, default=DEFAULT_KEYWORD, help="keyword to search")
+    args = parser.parse_args()
+    searchUrl = SITE_URL + 'search?q="' + args.keyword + '"&sort=new'
+
+    results = getSearchResults(searchUrl)
+    processResults(results) 
