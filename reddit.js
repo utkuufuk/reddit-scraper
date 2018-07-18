e5fe6ab9048c92c89caaadb89a9330d42b01a5b9
utkuufuk <utkuufuk@gmail.com>
2018-07-18 22:17:08 +0300

    display posts of related keyword on button click

--- reddit.js
+++ reddit.js
@@ -1,23 +1,24 @@
 'use strict';
 
 $(function() {
-    var $posts = $('#posts');
-    var $keywords = $('#keywords');
+    let $keywords = $('#keywords');
 
-    function addPost(post) {
-        $posts.append('<li><strong>Title:</strong> ' + post.title + '<br>' +
-                      '<strong>Author:</strong> ' + post.author + '<br>' +
-                      '<strong>Score:</strong> ' + post.score + '<br>' +
-                      '<strong>Date:</strong> ' + post.date + '<br>' +
-                      '<strong>Subreddit:</strong> ' + post.subreddit + '<br>' +
-                      '<strong>URL:</strong> ' + post.url + '<br>' +
+    function addPost(post, $posts) {
+        $posts.append('<li><strong>Title: </strong>' + post.title + '<br>' +
+                      '<strong>Author: </strong>' + post.author + '<br>' +
+                      '<strong>Score: </strong>' + post.score + '<br>' +
+                      '<strong>Date: </strong>' + post.date + '<br>' +
+                      '<strong>Subreddit: </strong>' + post.subreddit + '<br>' +
+                      '<strong>URL: </strong>' + post.url + '<br>' +
                       '<button data-id=' + post.id + ' class="expand">Show Comments</button></li>');
     }
 
     function addKeyword(keyword, product) {
-        $keywords.append('<br><strong>' + keyword + '</strong><br>');
-        $keywords.append('<li><strong>Last Crawl Date:</strong>' + product[keyword]['timestamp'] + '</li>');
-        $keywords.append('<li><strong>Subreddit: </strong> r/' + product[keyword]['subreddit'] + '</li>');
+        $keywords.append('<li><strong>' + keyword + '</strong><br>' +
+                         '<strong>Last Crawl Date: </strong>' + product[keyword]['timestamp'] + '<br>' +
+                         '<strong>Subreddit: </strong> r/' + product[keyword]['subreddit'] + '<br>' +
+                         '<button data-id="' + keyword + '" class="show-posts">Show Posts</button>' +
+                         '<ul class="posts"></ul></li>');
     }
 
     $.ajax({
@@ -31,5 +32,23 @@ $(function() {
         error: function() {
             alert('error loading keywords');
         }
+    });
+
+    $keywords.on('click', '.show-posts', function() {
+        let $posts = $(this).closest('li').find('.posts');
+        let keyword = $(this).attr('data-id');
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:3000/' + keyword + '/',
+            success: function(product) {
+                $.each(product['posts'], function(i, post) {
+                    addPost(post, $posts);
+                });
+            },
+            error: function() {
+                alert('error loading posts');
+            }
+        });
     });
 });
