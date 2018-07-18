64d72145beefeb3e65fed143b9a80d44777854ad
utkuufuk <utkuufuk@gmail.com>
2018-07-18 15:45:26 +0300

    display every search keyword in the database json file

--- reddit.js
+++ reddit.js
@@ -3,9 +3,7 @@
 $(function() {
     // cache some HTML elements
     var $posts = $('#posts');
-    var $name = $('#name');
-    var $drink = $('#drink');
-    var $id = $('#id');
+    var $keywords = $('#keywords');
 
     // creates an order <li> in the HTML document
     function addPost(post) {
@@ -15,64 +13,25 @@ $(function() {
                        '<strong>Date:</strong> ' + post.date + '<br>' +
                        '<strong>Subreddit:</strong> ' + post.subreddit + '<br>' +
                        '<strong>URL:</strong> ' + post.url + '<br>' +
-                       '<strong>Comments Count:</strong> ' + post.comments.count + '<br>' +
                        '<button data-id=' + post.id + ' class="expand">Show Comments</button></li>');
+    }
+
+    function addKeyword(keyword) {
+        $keywords.append('<li><strong>' + keyword + '</strong><br>' + '</li>');
     }
 
     // fetch & display all posts
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:3000/product',
-        success: function(posts) {
-            // write retrieved posts into HTML
-            $.each(posts, function(i, post) {
-                addPost(post);
+        url: 'http://localhost:3000/db/',
+        success: function(product) {
+            // write retrieved keywords into HTML
+            $.each(Object.keys(product), function(i, keyword) {
+                addKeyword(keyword);
             });
         },
         error: function() {
-            alert('error loading posts');
+            alert('error loading keywords');
         }
     });
-
-/*
-    // create a POST request to place an order whenever the ADD button is clicked
-    $('#add-order').on('click', function() {
-        // create an order object to send to backend
-        var order = {
-            name: $name.val(),
-            drink: $drink.val(),
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/orders',
-            data: order,
-            success: function(newOrder) {
-                addOrder(newOrder);
-            },
-            error: function() {
-                alert('error saving order');
-            }
-        });
-    });
-
-    // $('.remove').on.('click') won't work because AJAX is asynchronous!!!
-    $orders.on('click', '.remove', function() {
-        // cache the <li> and 'data-id' attribute associated with the button
-        var $li = $(this).closest('li');
-        var dataId = $(this).attr('data-id');
-
-        // send a DELETE request whenever the X button is clicked
-        $.ajax({
-            type: 'DELETE',
-            url: 'http://localhost:3000/orders/' + dataId,
-            success: function() {
-                $li.fadeOut(500);
-            },
-            error: function() {
-                alert('error deleting order');
-            }
-        });
-    });
-*/
 });
